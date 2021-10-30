af3fdcd213baa69c9b97523f1e90de0f2ed87ce4
NarendraV <vogireddy@gmail.com>
2021-10-30 15:32:10 +0100

    added conditional statement

--- index.php
+++ index.php
@@ -21,6 +21,14 @@
     foreach($products as $key => $value){
       echo "<p>A ".$key." costs £".$value.".</p>";
     }
+
+    echo "<h2>Items you can afford</h2>";
+
+    foreach($products as $key => $value){
+      if($value <= $credit){
+        echo "<p>".$key."</p>";
+      }
+    }
     ?>
   </body>
 </html>
