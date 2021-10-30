8ec9f575f569fda1c02d7c48030bae851c76e663
NarendraV <vogireddy@gmail.com>
2021-10-30 15:46:23 +0100

    Final code

--- index.php
+++ index.php
@@ -18,29 +18,26 @@
     $products['iPhone'] = 1000;
     $products['Toaster'] = 75;
 
-    foreach($products as $key => $value){
-      echo "<p>A ".$key." costs £".$value.".</p>";
-    }
-
-    echo "<h2>Items you can afford</h2>";
-
-    foreach($products as $key => $value){
-      if($value <= $credit){
-        echo "<p>".$key."</p>";
-      }
-    }
-
-    $amount=800;
     $taxRate=0.0825;
-    $addedTax=$amount*$taxRate;
-    echo $addedTax;
-    echo "<br>";
-    echo tax_calc(750,0.223);
 
     function tax_calc($amount,$tax){
       $calculate_tax = $amount*$tax;
       $amount=round($amount+$calculate_tax,2);
       return $amount;
+    }
+
+    foreach($products as $key => $value){
+      $costWithTax = tax_calc($value,$taxRate);
+      echo "<p>A ".$key." costs £".$costWithTax." with tax.</p>";
+    }
+
+    echo "<h2>Items you can afford</h2>";
+
+    foreach($products as $key => $value){
+      $costWithTax = tax_calc($value,$taxRate);
+      if($costWithTax <= $credit){
+        echo "<p>".$key."</p>";
+      }
     }
 
 
